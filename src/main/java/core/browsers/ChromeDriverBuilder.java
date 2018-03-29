8d5b71af7948ef83edd54c72522f6111b4edab00
Nastya <nastya.kuzorova@gmail.com>
2018-03-29 19:54:53 +0300

    ChromeDriverBuilder class added

---
+++ src/main/java/core/browsers/ChromeDriverBuilder.java
@@ -0,0 +1,31 @@
+package core.browsers;
+
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChromeDriverBuilder {
+
+    private ChromeOptions options = new ChromeOptions();
+
+    public ChromeDriverBuilder setMobileEmulation(String deviceName){
+        Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", deviceName);
+        options.setExperimentalOption("mobileEmulation", mobileEmulation);
+        return this;
+    }
+
+    public ChromeDriver build(){
+
+        return new ChromeDriver(options);
+    }
+
+    public ChromeDriverBuilder setPageLoadStrategy(String strategy){
+        this.options.setPageLoadStrategy(PageLoadStrategy.fromString(strategy));
+        return this;
+    }
+}
+
